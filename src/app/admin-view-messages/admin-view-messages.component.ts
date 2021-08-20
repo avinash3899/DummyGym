c0bc6b80b4510ed6223dc75f44b7bf09fda08de0
Avinash Sinha <avinash3899@gmail.com>
2021-08-20 22:30:31 +0530

    navbars final

---
+++ src/app/admin-view-messages/admin-view-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-messages',
+  templateUrl: './admin-view-messages.component.html',
+  styleUrls: ['./admin-view-messages.component.css']
+})
+export class AdminViewMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/admin-view-users/admin-view-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-users',
+  templateUrl: './admin-view-users.component.html',
+  styleUrls: ['./admin-view-users.component.css']
+})
+export class AdminViewUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
